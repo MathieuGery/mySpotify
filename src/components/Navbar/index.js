ceb7508dc1281e519c15a910d44396d09daaed9e
mathieugery <mathieu.gery@epitech.eu>
2021-03-29 16:17:15 +0200

    Edit NavBar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {Link} from "react-router-dom";
 import {connect} from "react-redux";
 import {logout} from "../../redux/actions/auth.action";
 import Cookies from 'js-cookie'
-
 
 const mapDispatchToProps = dispatch => ({
     logout: () => dispatch(logout())
@@ -21,34 +20,85 @@ function Navbar(props) {
     if (!Cookies.get('access_token') || !props.access_token)
         return <></>
     return (
-        <div className="w-screen h-20 bg-black flex items-center justify-between shadow-xl">
-            <div className="flex">
-                <Link to={"/me"}>
-                    <div className="ml-16 text-white text-lg hover:text-gray-300">
-                        Profile
+            <nav className="bg-gray-800 rounded-xl mx-10">
+                <div className="mx-auto px-2 sm:px-6 lg:px-8">
+                    <div className="relative flex items-center justify-between h-16">
+                        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                            <button type="button"
+                                    className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                                    aria-controls="mobile-menu" aria-expanded="false">
+                                <span className="sr-only">Open main menu</span>
+                                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                     viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                          d="M4 6h16M4 12h16M4 18h16"/>
+                                </svg>
+                                <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                     viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                          d="M6 18L18 6M6 6l12 12"/>
+                                </svg>
+                            </button>
+                        </div>
+                        <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                            <div className="flex-shrink-0 flex items-center">
+                                <img className="block lg:hidden h-8 w-auto"
+                                     src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" alt="Spotify-Logo"/>
+                                    <img className="hidden lg:block h-8 w-auto"
+                                         src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg"
+                                         alt="Spotify-Logo"/>
+                            </div>
+                            <div className="hidden sm:block sm:ml-6">
+                                <div className="flex space-x-4">
+                                    <Link to={"/main"}>
+                                        <div className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                                            Browse
+                                        </div>
+                                    </Link>
+
+                                    <Link to={"/my-playlist"}>
+                                        <div className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                                            Playlist
+                                        </div>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                            <div className="ml-3 relative">
+                                <div>
+                                    <button type="button"
+                                            className="text-white bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                                            id="user-menu" aria-expanded="false" aria-haspopup="true" onClick={() => { props.logout() }}>
+                                        <span className="sr-only">Log Out</span>
+                                        <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                             viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                  d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+
+                            <div className="ml-3 relative">
+                                <div>
+                                    <button type="button"
+                                            className="text-white bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                                            id="user-menu" aria-expanded="false" aria-haspopup="true">
+                                        <span className="sr-only">Log Out</span>
+                                        <svg className="h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                             viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                  d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </Link>
-                <Link to={"/main"}>
-                    <div className="ml-16 text-white text-lg hover:text-gray-300">
-                        Browse
-                    </div>
-                </Link>
-                <Link to={"/my-playlist"}>
-                    <div className="ml-16 text-white text-lg hover:text-gray-300">
-                        Playlist
-                    </div>
-                </Link>
-            </div>
-            <div className="w-10 h-10 mr-10">
-                <svg onClick={() => {
-                    props.logout()
-                }} className="text-white hover:text-gray-300" xmlns="http://www.w3.org/2000/svg" fill="none"
-                     viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                          d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
-                </svg>
-            </div>
-        </div>
+                </div>
+            </nav>
     );
 }
 
