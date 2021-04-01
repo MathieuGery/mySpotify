c9fb93731833ca761e7222f7dca0e4f4e674eb01
Shegaie <gregoire.lanfranchini@epitech.eu>
2021-04-01 11:32:41 +0200

    add dynamic icon on player

--- src/components/Main/Player.js
+++ src/components/Main/Player.js
@@ -15,7 +15,7 @@ function Player(props) {
     }, []);
 
     function computeWidthProgress() {
-        return Math.ceil(props.playing?.progress_ms*100/props.playing?.playing_track?.duration_ms) + "%";
+        return Math.ceil(props.playing?.progress_ms * 100 / props.playing?.playing_track?.duration_ms) + "%";
     }
 
     const buildPlayer = () => {
@@ -61,12 +61,20 @@ function Player(props) {
                                     <path d="M4 5h3v10H4V5zm12 0v10l-9-5 9-5z"/>
                                 </svg>
                             </div>
-                            <div className="text-dark p-8 rounded-full bg-red-light shadow-lg">
-                                <svg className="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
-                                     viewBox="0 0 20 20">
-                                    <path d="M5 4h3v12H5V4zm7 0h3v12h-3V4z"/>
-                                </svg>
-                            </div>
+                            {props.playing?.is_playing &&
+                            <svg xmlns="http://www.w3.org/2000/svg" className="w-24 h-24 hover:text-gray-700" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd"
+                                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                      clipRule="evenodd"/>
+                            </svg>
+                            }
+                            {!props.playing?.is_playing &&
+                            <svg xmlns="http://www.w3.org/2000/svg" className="w-24 h-24 hover:text-gray-700" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd"
+                                      d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
+                                      clipRule="evenodd"/>
+                            </svg>
+                            }
                             <div className="text-grey-darker">
                                 <svg className="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
                                      viewBox="0 0 20 20">
@@ -81,7 +89,8 @@ function Player(props) {
                             </div>
                             <div className="mt-1">
                                 <div className="h-1 bg-gray-300 rounded-full">
-                                    <div style={{width: computeWidthProgress()}} className="w-1/5 h-1 bg-primary rounded-full relative">
+                                    <div style={{width: computeWidthProgress()}}
+                                         className="w-1/5 h-1 bg-primary rounded-full relative">
                                     </div>
                                 </div>
                             </div>
